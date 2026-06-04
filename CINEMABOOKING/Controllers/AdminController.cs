ce58a2d45b834ee9c4731df0eb86148d04550cb4
hnam <hnampython124@gmail.com>
2026-06-04 20:32:14 +0700

    Update admin UI

--- CINEMABOOKING/Controllers/AdminController.cs
+++ CINEMABOOKING/Controllers/AdminController.cs
@@ -12,5 +12,85 @@ namespace CINEMABOOKING.Controllers
         {
             return View();
         }
+
+        public ActionResult Movies()
+        {
+            return View();
+        }
+
+        public ActionResult Cinemas()
+        {
+            return View();
+        }
+
+        public ActionResult Rooms()
+        {
+            return View();
+        }
+
+        public ActionResult Showtimes()
+        {
+            return View();
+        }
+
+        public ActionResult Users()
+        {
+            return View();
+        }
+
+        public ActionResult Bookings()
+        {
+            return View();
+        }
+
+        public ActionResult Tickets()
+        {
+            return View();
+        }
+
+        public ActionResult Payments()
+        {
+            return View();
+        }
+
+        public ActionResult Reports()
+        {
+            return View();
+        }
+
+        public ActionResult Banners()
+        {
+            return View();
+        }
+
+        public ActionResult Promotions()
+        {
+            return View();
+        }
+
+        public ActionResult News()
+        {
+            return View();
+        }
+
+        public ActionResult Reviews()
+        {
+            return View();
+        }
+
+        public ActionResult SupportTickets()
+        {
+            return View();
+        }
+
+        public ActionResult ActivityLogs()
+        {
+            return View();
+        }
+
+        public ActionResult Settings()
+        {
+            return View();
+        }
     }
 }
